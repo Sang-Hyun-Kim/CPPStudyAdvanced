bc527ff77820ecba9faee86e47c68dc5b95d3cfa
Beomin <960322ksh@hanmail.net>
2023-09-09 04:42:16 +0900

    초기 커밋.

---
+++ Funpointer/Funpointer.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+// 함수 포인터 
+
+int main()
+{
+
+	typedef int(FUNC_TYPE)(int a, int b); // int a,b를 인자로 갖는 int 반환형의 함수를 (FUNC_TYPE) 이라고 typedef
+	using FUNC_TYPE2 = int(int a, int b);
+
+}
+
